ef2e27936043632a23db2c477d565f85ad873f8a
elhachemi <elhachemiadmane@gmail.com>
2021-10-22 10:50:13 +0100

    loading_butom

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -6,14 +6,17 @@ class Search extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            term: ""
+            term: "",
+            is_loading: false
         }
     }
 
     onClickSeach = async (e) => {
         let value = this.state.term;
+        this.setState({is_loading: true});//les flagues 
         let resp = await fetch(`https://itunes.apple.com/search?term=${value}`);
         let data = await resp.json();
+        this.setState({is_loading: false});
         this.props.callbackFetch(data);
     }
 
@@ -32,7 +35,14 @@ class Search extends Component {
                 <Input size="large" placeholder="Ex: .. Hello Adele" prefix={<AppleFilled />} style={{width: 500}} onChange={e => this.setState({term: e.target.value})} value={this.state.term}/>
                 <br/>
                 <br/>
-                 <Button onClick={e => this.onClickSeach(e)} type="primary" shape="round" icon={<AppleFilled />} size="large" style={{width: 300}}>
+                 <Button 
+                 onClick={e => this.onClickSeach(e)} 
+                 type="primary" 
+                 shape="round" 
+                 icon={<AppleFilled />} 
+                 size="large" 
+                 style={{width: 300}} 
+                 loading={this.state.is_loading}>
                 Search my song
                 </Button>
             </div>
