fabcbce91a3606a2f125e7c9346985227123bb87
Kenshinvag <nasro@imadrassa.com>
2021-10-22 12:42:52 +0100

    Refactoring.

--- src/components/singleResult.jsx
+++ src/components/singleResult.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
-
+import { Card } from 'antd'
 class SingleResult extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <Card
+                hoverable
+                style={{ 
+                    width: 200,
+                    height: 300,
+                    margin: 10
+                }}
+                cover={<img src={this.props.picture} />}
+            >
+                <Card.Meta title={this.props.song} description={this.props.singer}/>
+            </Card>
         )
     }
 }
