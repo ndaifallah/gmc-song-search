f0dd587b8b65c4beccf1d122661fef26bf6866b5
elhachemi <elhachemiadmane@gmail.com>
2021-10-22 12:25:11 +0100

    afficher resultats

--- src/components/result.jsx
+++ src/components/result.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
+import { Card } from 'antd';
 
 class Result extends Component {
+    
     render() {
+        console.log(this.props.data);
         return (
-            <div>
-                
+            //pour afficher html
+            // 1 <div>
+           /// 2  {this.props.data.map(el=><h3>{el.trackName}</h3>)}
+       //  3 </div>
+
+            //on l'affiche dans une card voir ant designe
+            <div style={{
+                display: "flex",
+                flexDirection:"row",
+                flexWrap:"wrap",
+                padding: 20
+            }}>
+                {this.props.data.map(el=><Card
+
+    hoverable
+    style={{ width: 200,
+        height: 300,
+    margin: 10,
+    }}
+    cover={<img src={el.artworkUrl100} />}
+  >
+    <Card.Meta title={el.trackName} description={el.artistName} />
+  </Card>)}
             </div>
         )
     }
