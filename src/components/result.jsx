8e0a976d781454ac2408f8a778388cdb3c3a0855
elhachemi <elhachemiadmane@gmail.com>
2021-10-22 15:25:26 +0100

    avec boton afficher plus

--- src/components/result.jsx
+++ src/components/result.jsx
@@ -1,8 +1,15 @@
+import { Button } from 'antd';
 import React, { Component } from 'react'
 import SingleResult from './singleResult';
 
 
 class Result extends Component {
+    constructor (props){
+        super (props);
+        this.state={
+            count: 12
+        }
+    }
     
     render() {
         console.log(this.props.data);
@@ -13,13 +20,18 @@ class Result extends Component {
        //  3 </div>
 
             //on l'affiche dans une card voir ant designe
+            <div> 
             <div style={{
                 display: "flex",
                 flexDirection:"row",
                 flexWrap:"wrap",
                 padding: 20
             }}>
-                {this.props.data.map(el=> <SingleResult song={el.trackName} singer ={el.artistName} picture={el.artworkUrl100} />)}
+                {this.props.data.map((el, idx)=> idx<this.state.count? <SingleResult song={el.trackName} singer ={el.artistName} picture={el.artworkUrl100}  />: <></>)}
+</div> 
+<Button type="linK" style={{marginBottom: 50}} onClick={e=>this.setState({count: this.state.count+6})}> 
+afficher plus de résultat
+</Button>
             </div>
         )
     }
